d078433b258252e1f04a65dd5673d7ecceb30469
zhehong0627 <80819271+zhehong0627@users.noreply.github.com>
2021-05-19 16:51:24 +0800

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -26,7 +26,7 @@ function preload() {
 }
 
 function setup() {
-  createCanvas(320, 260);
+  createCanvas(320, 320);
   // Create the video
   var constraints = {
 
@@ -49,7 +49,7 @@ function setup() {
     //}
 
   };
-  video = createCapture(VIDEO);
+  video = createCapture(constraints);
   video.size(320, 320);
   video.hide();
 
